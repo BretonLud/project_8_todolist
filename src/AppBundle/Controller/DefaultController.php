dbacc06c5b6b2ff3804f81084ecf4f26818a5e64
BretonLud <godneo@hotmail.fr>
2024-10-25 15:16:13 +0200

    Refactor routing and response in DefaultController
    Replaced SensioFrameworkExtraBundle's Route annotation with Symfony's Routing component. Modified `indexAction` to explicitly return a Response object for better type safety.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,15 +2,16 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction()
+    public function indexAction(): Response
     {
         return $this->render('default/index.html.twig');
     }
