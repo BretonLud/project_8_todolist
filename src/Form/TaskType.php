d6c877cfd5e3439459804a04dee660f56d643dde
BretonLud <godneo@hotmail.fr>
2024-11-08 20:46:41 +0100

    Refactor: Move form types to a new directory
    Moved UserType and TaskType from AppBundle/Form to Form. Updated namespaces accordingly to match new file locations. Simplified TaskType by adjusting import order.

--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace AppBundle\Form;
+namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TaskType extends AbstractType
@@ -24,8 +24,7 @@ class TaskType extends AbstractType
                 'constraints' => [
                     new NotBlank(),
                 ]
-            ])
-            //->add('author') ===> must be the user authenticated
+            ])//->add('author') ===> must be the user authenticated
         ;
     }
 }
