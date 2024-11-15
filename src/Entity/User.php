1677f5eadf8f0bc1b0051cf94db83302bdbf5d6e
BretonLud <godneo@hotmail.fr>
2024-11-15 19:55:32 +0100

    Add type hints to entity methods and form builders
    Updated the methods in Task, User, and form builder classes to include type hints for their parameters and return types. This enhances code readability and enables better static analysis.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -89,7 +89,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return array('ROLE_USER');
     }
     
-    public function eraseCredentials()
+    public function eraseCredentials(): void
     {
     }
     
