edec1237cf5e7ea09747015bec1db4aa1b7dd6a1
BretonLud <godneo@hotmail.fr>
2024-11-15 19:20:25 +0100

    Refactor TaskController to use TaskService
    Introduce TaskService to handle task operations, replacing direct Doctrine calls in TaskController. Add TaskRepository to encapsulate persistence logic.

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Task;
 use App\Form\TaskType;
+use App\Service\TaskService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,12 +13,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
+    public function __construct(private readonly TaskService $taskService)
+    {
+    }
+    
     /**
      * @Route("/tasks", name="task_list")
      */
     public function listAction(): Response
     {
-        return $this->render('task/list.html.twig', ['tasks' => $this->getDoctrine()->getRepository('App:Task')->findAll()]);
+        
+        $tasks = $this->taskService->getAll();
+        return $this->render('task/list.html.twig', ['tasks' => $tasks]);
     }
     
     /**
@@ -31,10 +38,7 @@ class TaskController extends AbstractController
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            
-            $em->persist($task);
-            $em->flush();
+            $this->taskService->save($task);
             
             $this->addFlash('success', 'La tâche a été bien été ajoutée.');
             
@@ -54,7 +58,7 @@ class TaskController extends AbstractController
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $this->taskService->save($task);
             
             $this->addFlash('success', 'La tâche a bien été modifiée.');
             
@@ -73,7 +77,7 @@ class TaskController extends AbstractController
     public function toggleTaskAction(Task $task): RedirectResponse
     {
         $task->toggle(!$task->isDone());
-        $this->getDoctrine()->getManager()->flush();
+        $this->taskService->save($task);
         
         $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
         
@@ -85,9 +89,7 @@ class TaskController extends AbstractController
      */
     public function deleteTaskAction(Task $task): RedirectResponse
     {
-        $em = $this->getDoctrine()->getManager();
-        $em->remove($task);
-        $em->flush();
+        $this->taskService->remove($task);
         
         $this->addFlash('success', 'La tâche a bien été supprimée.');
         
