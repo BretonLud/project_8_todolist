778c25f175dac16b515f829c6226f1c649a5aef7
BretonLud <godneo@hotmail.fr>
2024-11-15 19:19:28 +0100

    Refactor authentication logic
    Replaced manual authentication handling in SecurityController with a new LoginFormAuthenticator class. This modularizes the authentication process and cleans up the controller by moving the login logic into a dedicated authenticator service.

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,40 +3,25 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="login")
-     */
-    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils): Response
+    #[Route(path: '/login', name: 'app_login')]
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
         
-        return $this->render('security/login.html.twig', array(
-            'last_username' => $lastUsername,
-            'error' => $error,
-        ));
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
     
-    /**
-     * @Route("/login_check", name="login_check")
-     */
-    public function loginCheck(): void
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
     {
-        // This code is never executed.
-    }
-    
-    /**
-     * @Route("/logout", name="logout")
-     */
-    public function logoutCheck(): void
-    {
-        // This code is never executed.
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
