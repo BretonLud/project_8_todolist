831143048badd94dae6461ee014ca14af8930bda
BretonLud <godneo@hotmail.fr>
2024-11-16 20:32:35 +0100

    Add __toString method to User entity
    This method returns the username of the User object, enabling the object to be used as a string representation of the username. This change improves the usability of the User entity in contexts where string representations are needed.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -81,4 +81,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function eraseCredentials(): void
     {
     }
+    
+    public function __toString()
+    {
+        return $this->username;
+    }
 }
