bf39629734348a0e27be1d0bd56108b433d553e5
BretonLud <godneo@hotmail.fr>
2024-11-17 02:43:14 +0100

    Add user-related methods to TaskService
    Introduced methods to find tasks by user and by different users, and added a method to update task status. This enhances the functionality to better manage tasks based on user permissions and status changes.

--- src/Service/TaskService.php
+++ src/Service/TaskService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 
 use App\Entity\Task;
 use App\Repository\TaskRepository;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 readonly class TaskService
 {
@@ -16,13 +17,29 @@ readonly class TaskService
         return $this->taskRepository->findAll();
     }
     
+    public function remove(Task $task): void
+    {
+        $this->taskRepository->remove($task);
+    }
+    
+    public function updateTaskStatus(Task $task): void
+    {
+        $task->toggle(!$task->isDone());
+        $this->save($task);
+    }
+    
     public function save(Task $task): void
     {
         $this->taskRepository->save($task);
     }
     
-    public function remove(Task $task): void
+    public function findTasksByUser(UserInterface $user, bool $accessGranted): array
     {
-        $this->taskRepository->remove($task);
+        return $this->taskRepository->findTasksByUser($user, $accessGranted);
+    }
+    
+    public function findTasksByDifferentUsers(UserInterface $user)
+    {
+        return $this->taskRepository->findTasksByDifferentUsers($user);
     }
 }
