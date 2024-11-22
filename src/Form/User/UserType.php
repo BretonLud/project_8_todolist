7bd2d2adec1329e3c53e55dbec6a77e76d656df6
BretonLud <godneo@hotmail.fr>
2024-11-22 19:29:57 +0100

    Refactor UserType and adjust its namespace
    Renamed `UserType` to `User/UserType` to organize form types. Removed password fields and added `OptionsResolver` configuration to set the `User` entity as the data class.

--- src/Form/User/UserType.php
+++ src/Form/User/UserType.php
@@ -1,13 +1,13 @@
 <?php
 
-namespace App\Form;
+namespace App\Form\User;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
@@ -22,21 +22,18 @@ class UserType extends AbstractType
                     new NotBlank(),
                 ]
             ])
-            ->add('password', RepeatedType::class, [
-                'type' => PasswordType::class,
-                'invalid_message' => 'Les deux mots de passe doivent correspondre.',
-                'required' => true,
-                'first_options' => ['label' => 'Mot de passe'],
-                'second_options' => ['label' => 'Tapez le mot de passe à nouveau'],
-                'constraints' => [
-                    new NotBlank(),
-                ]
-            ])
             ->add('email', EmailType::class, [
                 'label' => 'Adresse email',
                 'constraints' => [
                     new NotBlank(),
                 ]
             ]);
+    }
+    
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
     }
 }
