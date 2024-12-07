679db18b27e403653b616bc27d11f8967a5a8204
BretonLud <godneo@hotmail.fr>
2024-12-07 01:34:36 +0100

    Add tests for successful authentication redirects
    Updated existing tests by adding return type declarations. Introduced new tests to verify redirection upon successful authentication, including redirects to the homepage and specific target paths. These changes help ensure that the authentication process functions as expected and routes users appropriately after logging in.

--- tests/Controller/SecurityControllerTest.php
+++ tests/Controller/SecurityControllerTest.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class SecurityControllerTest extends WebTestCase
 {
-    public function testLoginPageRendersCorrectly()
+    public function testLoginPageRendersCorrectly(): void
     {
         $client = static::createClient();
         $client->request('GET', '/login');
@@ -18,7 +18,7 @@ class SecurityControllerTest extends WebTestCase
         $this->assertSelectorExists('button[type="submit"]');
     }
     
-    public function testRedirectIfAlreadyLoggedIn()
+    public function testRedirectIfAlreadyLoggedIn(): void
     {
         $client = static::createClient();
         $container = $client->getContainer();
@@ -37,7 +37,7 @@ class SecurityControllerTest extends WebTestCase
         $this->assertResponseRedirects('/');
     }
     
-    public function testLoginWithInvalidCredentialsShowsError()
+    public function testLoginWithInvalidCredentialsShowsError(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/login');
@@ -58,5 +58,73 @@ class SecurityControllerTest extends WebTestCase
         
         // Assurez-vous qu'une erreur est affichée
         $this->assertSelectorExists('.alert.alert-danger'); // Assume there's a class 'error' for displaying errors
+    }
+    
+    
+    public function testAuthenticationSuccessRedirectsHomepage(): void
+    {
+        $client = static::createClient();
+        
+        $container = $client->getContainer();
+        $manager = $container->get('doctrine')->getManager();
+        
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setUsername('testuser');
+        $user->setPassword(password_hash('<PASSWORD>', PASSWORD_BCRYPT));
+        $manager->persist($user);
+        $manager->flush();
+        
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Sign in')->form([
+            'username' => 'testuser',
+            'password' => '<PASSWORD>',
+        ]);
+        
+        $client->submit($form);
+        
+        $targetPath = '/tasks';
+        
+        $response = $client->getResponse();
+        $redirectUrl = $response->headers->get('Location');
+        
+        if ($redirectUrl === $targetPath) {
+            $this->assertResponseRedirects($targetPath);
+        } else {
+            $this->assertResponseRedirects('/');
+        }
+        
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+    
+    public function testAuthenticationSuccessRedirectTargetPath(): void
+    {
+        $client = static::createClient();
+        $session = $client->getContainer()->get('session.factory')->createSession();
+        $container = $client->getContainer();
+        $manager = $container->get('doctrine')->getManager();
+        
+        $user = new User();
+        $user->setEmail('test@example.com');
+        $user->setUsername('testuser');
+        $user->setPassword(password_hash('<PASSWORD>', PASSWORD_BCRYPT));
+        $manager->persist($user);
+        $manager->flush();
+        
+        $client->request('GET', '/tasks');
+        $session->set('_security.main.target_path', '/tasks');
+        $session->save();
+        
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('Sign in')->form([
+            'username' => 'testuser',
+            'password' => '<PASSWORD>',
+        ]);
+        
+        $client->submit($form);
+        $this->assertResponseRedirects($session->get('_security.main.target_path'));
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
     }
 }
