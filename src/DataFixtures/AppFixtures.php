8882d3c06e744c3b0fd77c99c2772053cdc39fff
BretonLud <godneo@hotmail.fr>
2024-12-14 17:35:15 +0100

    Add user and task fixtures in AppFixtures
    Introduced the creation of admin, users, and tasks in AppFixtures to populate the database with sample data. Leveraged UserService and TaskService for entity persistence and password encoding. This update enhances the development environment with realistic test data.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,63 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Task;
+use App\Entity\User;
+use App\Service\TaskService;
+use App\Service\UserService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * @codeCoverageIgnore
+ */
 class AppFixtures extends Fixture
 {
+    public function __construct(private readonly UserService $userService, private readonly TaskService $taskService)
+    {
+    }
+    
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
+        $users = [];
+        
+        $admin = new User();
+        $admin->setUsername('admin');
+        $admin->setEmail('admin@localhost.fr');
+        $admin->setPassword('admin.1234');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $this->userService->encoderPassword($admin);
+        $this->userService->save($admin);
+        
+        $users[] = $admin;
+        
+        $user = new User();
+        $user->setUsername('user');
+        $user->setEmail('user@localhost.fr');
+        $user->setPassword('user.1234');
+        $this->userService->save($user);
+        $this->userService->encoderPassword($user);
+        $users[] = $user;
+        
+        for ($i = 0; $i < 3; $i++) {
+            $user = new User();
+            $user->setUsername('user' . $i);
+            $user->setEmail('user' . $i . '@localhost.fr');
+            $user->setPassword('user' . $i . '.1234');
+            $this->userService->encoderPassword($user);
+            $this->userService->save($user);
+            $users[] = $user;
+        }
+        
+        for ($i = 0; $i < 100; $i++) {
+            $task = new Task();
+            $task->setTitle('Task ' . $i);
+            $task->setUser($users[rand(0, count($users) - 1)]);
+            $task->setContent('Content of task ' . $i);
+            
+            $this->taskService->save($task);
+        }
+        
+        
     }
 }
